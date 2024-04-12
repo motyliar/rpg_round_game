9fd497ca713f730add03c43303a0ccfe136fcbf3
motyliar <j.turostowski@gmail.com>
2024-04-12 10:33:01 +0200

    update import bug- type mismatch

--- app/src/main/java/pl/motyliar/budgetapp/domain/player/Human.kt
+++ app/src/main/java/pl/motyliar/budgetapp/domain/player/Human.kt
@@ -1,5 +1,7 @@
 package pl.motyliar.budgetapp.domain.player
 
+import pl.motyliar.budgetapp.domain.character.Character
+
 class Human(character: Character) : Player(character) {
 
     override fun attack() {
