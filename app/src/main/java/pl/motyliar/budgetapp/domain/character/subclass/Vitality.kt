84d148e991fa5fa46850b338156e65d492130fa3
motyliar <j.turostowski@gmail.com>
2024-04-16 07:50:02 +0200

    creating more tests to vitality class

--- app/src/main/java/pl/motyliar/budgetapp/domain/character/subclass/Vitality.kt
+++ app/src/main/java/pl/motyliar/budgetapp/domain/character/subclass/Vitality.kt
@@ -5,7 +5,7 @@ import pl.motyliar.budgetapp.core.equatable.Equatable
 class Vitality(
     var strength: Int,
     var intelligence: Int,
-   private var life: Int,
+    private var life: Int,
     val magic: Int,
     val dexterity: Int,
     private var energy: Int,
@@ -14,37 +14,43 @@ class Vitality(
     ) {
 
     private var fatigue: Int = 0
+    val getFatigue get() = fatigue
     private var damages: Int = 0
+    val getDamage get() = damages
+
 
     val characterLife = life
     val characterEnergy = energy
-    val addLife : (Int) -> Unit = {this.life += it}
-    val addIntelligence: (Int) -> Unit = {this.intelligence += it }
+    val addLife: (Int) -> Unit = { this.life += it }
+    val addIntelligence: (Int) -> Unit = { this.intelligence += it }
 
     fun recoveringHealth(value: Int) {
-        if (characterLife + value >= characterLife) {
+        if (getCurrentLife() + value >= characterLife) {
             damages = 0
         } else {
             damages -= value
         }
     }
+
     fun recoveringEnergy(value: Int) {
-        if (energy + value >= characterEnergy ) {
+        if (getCurrentEnergy() + value >= characterEnergy) {
             fatigue = 0
         } else {
             fatigue -= value
         }
     }
+
     fun getCurrentEnergy(): Int {
-        if(energy - fatigue <= 0) return 0
+        if (energy - fatigue <= 0) return 0
         return energy - fatigue
     }
+
     fun getCurrentLife(): Int {
-        if(life - damages <= 0) return 0
+        if (life - damages <= 0) return 0
         return life - damages
     }
 
-    fun addDamages(value:Int) {
+    fun addDamages(value: Int) {
         damages += value
     }
 
--- app/src/test/java/pl/motyliar/budgetapp/domain/character/VitalityTests.kt
+++ app/src/test/java/pl/motyliar/budgetapp/domain/character/VitalityTests.kt
@@ -24,4 +24,60 @@ class VitalityTests {
 
         assertEquals(40, warrior.vitality.getCurrentLife())
     }
+
+    @Test
+    fun shouldAddIntelligenceValue() {
+        warrior.vitality.addIntelligence(10)
+
+        assertEquals(20, warrior.vitality.intelligence)
+    }
+
+    @Test
+    fun shouldReturnCurrentDamageValue() {
+        warrior.vitality.addDamages(20)
+        assertEquals(20, warrior.vitality.getDamage)
+    }
+    @Test
+    fun shouldReturnCurrentDamageAfterRecovering() {
+        warrior.vitality.addDamages(20)
+        warrior.vitality.recoveringHealth(10)
+        assertEquals(10, warrior.vitality.getDamage)
+
+    }
+
+    @Test
+    fun shouldReturnZeroDamageWhenIsOverRecovering() {
+        warrior.vitality.addDamages(20)
+        warrior.vitality.recoveringHealth(30)
+        assertEquals(0, warrior.vitality.getDamage)
+
+    }
+
+    @Test
+    fun shouldReturnCurrentFatigueValue() {
+        warrior.vitality.addFatigue(20)
+        assertEquals(20, warrior.vitality.getFatigue)
+    }
+    @Test
+    fun shouldReturnCurrentEnergy() {
+        warrior.vitality.addFatigue(5)
+        assertEquals(5, warrior.vitality.getCurrentEnergy())
+
+    }
+    @Test
+    fun shouldReturnCurrentFatigueAfterRecovering() {
+        warrior.vitality.addFatigue(10)
+        warrior.vitality.recoveringEnergy(5)
+        assertEquals(5, warrior.vitality.getFatigue)
+
+    }
+
+    @Test
+    fun shouldReturnZeroFatigueWhenIsOverRecovering() {
+        warrior.vitality.addFatigue(20)
+        warrior.vitality.recoveringEnergy(30)
+        assertEquals(0, warrior.vitality.getFatigue)
+
+    }
+
 }
